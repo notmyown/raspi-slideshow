842ad0f47f965e1085fa6096e4db265e00fe3e0b
Marco Bergen <bergen.marco@gmail.com>
2020-05-03 21:20:32 +0200

    original dateiname

--- config/install.sh
+++ config/install.sh
@@ -3,5 +3,5 @@ BASEDIR="$(dirname $(readlink -f $0))"
 
 sudo mkdir -p /home/pi/.config/autostart
 
-cp -fR ${BASEDIR}/feh_startup.desktop /home/pi/.config/autostart/
-cp -fR ${BASEDIR}/gpio_startup.desktop /home/pi/.config/autostart/
+cp -fR ${BASEDIR}/start_feh.desktop /home/pi/.config/autostart/
+cp -fR ${BASEDIR}/start_gpio.desktop /home/pi/.config/autostart/
