9b9ba1974ed3cbb5caf1c461f058689d298a8e4e
Marco Bergen <bergen.marco@gmail.com>
2020-05-03 20:42:01 +0200

    lokal

--- config/install.sh
+++ config/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+BASEDIR=$(dirname "$0")
 
 sudo mkdir -p ~/.config/autostart
-cp ./feh_startup.desktop ~/.config/autostart
-cp ./gpio_startup.desktop ~/.config/autostart
+cp ${BASEDIR}/feh_startup.desktop ~/.config/autostart
+cp ${BASEDIR}/gpio_startup.desktop ~/.config/autostart
