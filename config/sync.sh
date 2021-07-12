79c5e9a32478a27cf1fbcbfac014166c6e818ce2
Marco Bergen <bergen.marco@gmail.com>
2021-07-12 13:13:50 +0200

    update instead of pull

--- config/sync.sh
+++ config/sync.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 BASEDIR="$(dirname $(readlink -f $0))"
 cd ${BASEDIR}/..
-git pull
+git update
 
 mkdir -p ${BASEDIR}/../images
 sudo rclone sync -v gdrive:slideshow ${BASEDIR}/../images
