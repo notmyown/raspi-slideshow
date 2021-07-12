b0139a72b8210acfacd702ecece7ea2f73cdfb89
Marco Bergen <bergen.marco@gmail.com>
2021-07-12 13:12:02 +0200

    sort newest first

--- config/startup.sh
+++ config/startup.sh
@@ -2,6 +2,6 @@
 BASEDIR="$(dirname $(readlink -f $0))"
 
 ${BASEDIR}/sync.sh
-feh --hide-pointer --borderless --quit --slideshow-delay 10 --image-bg black --fullscreen --auto-zoom --reload 1 --recursive -Sfilename ${BASEDIR}/../images
+feh --hide-pointer --borderless --quit --slideshow-delay 10 --image-bg black --fullscreen --auto-zoom --reload 1 --sort mtime --recursive -Sfilename ${BASEDIR}/../images
 
 exit 0
